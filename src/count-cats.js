99484f7bbe88f250cd7c305f8b435aadd6e12ad4
Ирина Ирина <irin-ka@inbox.ru>
2021-01-27 12:18:57 +0300

    task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,17 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  //throw new CustomError('Not implemented');
-  const cat = '^^';
-  let count = 0;
 
+  let count = 0;
   matrix.forEach(element => {
     element.forEach(item => {
-      if (item === cat) {
-        count++;
-      }
-    });
-  });
-if (!count) throw new CustomError('Not implemented');
+      if (item === '^^') 
+              count++;
+    })
+  })
     return count;
 };
+
