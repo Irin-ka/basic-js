03f8b328c19c637164228940f670b5797c926c4b
Ирина Ирина <irin-ka@inbox.ru>
2021-01-27 15:16:02 +0300

    task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let arr = [];
+  members.forEach(element => {
+ if (typeof element === 'string') {
+   element = element.trim();
+   arr.push(element[0].toUpperCase());
+ }
+       })
+    return arr.sort().join('');
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error('');
+  let newArray = [];
+  let toggle = true;
+  arr.forEach(function (item, i) {
+
+    if (item === '--discard-next') { // удаляет следующий элемент
+      toggle = false;
+    } else if (item === '--discard-prev') { // удаляет предыдущий предыдущий
+      newArray.pop();
+    } else if (item === '--double-next') { // удваивает следующий элемент 
+      if (i < arr.length - 1) {
+        newArray.push(arr[i+1]);
+      }
+    } else if (item === '--double-prev') { // удваивает предыдущий элемент
+      if (i > 0) {
+        newArray.push(arr[i-1]);
+      }
+    } else if (toggle) {
+      newArray.push(item);
+    } else {
+      toggle = true;
+    }
+  });
+
+  return newArray;
 };
